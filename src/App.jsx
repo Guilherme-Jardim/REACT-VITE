a8e786102198876e61e30a5a26eb922d7252835a
guilherme-jardim <guilhermejardimj@gmail.com>
2024-07-29 11:45:42 -0300

    add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { Header } from './components/header'
+import { Header } from './components/Header'
 import { NamedExport } from './NamedExport'
+import { Sidebar } from './components/Sidebar'
+
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <NamedExport 
-        author="Propertie 1" 
-        content="Propertie 2" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <NamedExport 
+            author="Propertie 1" 
+            content="Propertie 2" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
