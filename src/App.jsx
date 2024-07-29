81dd5012661535bb35c5ff203027895d0b040a1c
guilherme-jardim <guilhermejardimj@gmail.com>
2024-07-29 10:04:07 -0300

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/header'
 import { NamedExport } from './NamedExport'
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <NamedExport 
         author="Propertie 1" 
         content="Propertie 2" 
